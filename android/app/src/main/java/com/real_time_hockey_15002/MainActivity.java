59e2624ad9e553edf3750b17e1814578c36040bf
Crowdbotics <team@crowdbotics.com>
2020-03-23 22:17:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/real_time_hockey_15002/MainActivity.java
@@ -0,0 +1,15 @@
+package com.real_time_hockey_15002;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "real_time_hockey_15002";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,7 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://real-time-hockey-15002.botics.co",
+  defaultTimeout: 5000,
+  pubPublishKey: 'pub-c-09038c51-029c-47a7-95f2-b6c7407afb12',
+  pubSubscribeKey: 'sub-c-6b1bee04-ba9f-11e9-8753-ce76e7dc5905'
+};
